7aef1f2670160d74d357354d4a1dc7c50a9166c6
Joel Acquah <joelaj06@gmail.com>
2026-02-19 19:29:42 +0000

    fixed customer population and validation error

--- src/validation/customerSchema.ts
+++ src/validation/customerSchema.ts
@@ -6,6 +6,7 @@ export const customerQuerySchema = baseQuerySchema.extend({
   phone: z.string().max(13).min(10),
   email: z.email(),
   companyName: z.string(),
+  softwareId: z.string(),
   dateConverted: z.string().optional(),
   location: z.string().optional(),
   status: z.enum(["active", "inactive"]).optional(),
